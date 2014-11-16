e7dcd3c72ac4b9be794c54dc37d1c191707b55da
samohtj <thomas.jonathan.c@gmail.com>
2014-11-15 20:56:22 -0500

    Documentation
    Added comments all over the place

--- Robot Code/Autonomous/headers/AutoFunctions.h
+++ Robot Code/Autonomous/headers/AutoFunctions.h
@@ -1,3 +1,4 @@
+#include "Multiplexer.h"
 
 /*
 *
@@ -7,31 +8,54 @@
 void initializeRobot();
 task autonomous();
 
+/*
+*	allMotorsTo
+*	Set all the motors to the input value
+*/
 void allMotorsTo(int i)
 {
-		motor[mDriveLeft] = i;
-		motor[mDriveRight] = i;
-		motor[mBrush] = i;
-		motor[mLift] = i;
+		motor[mDriveLeft] 	= i;
+		motor[mDriveRight] 	= i;
+		motor[mBrush] 		= i;
+		motor[mLift] 		= i;
 }
 
+/*
+*	initializeRobot
+*	Move all motors and servos to their starting positions
+*/
 void initializeRobot()
 {
 
 }
 
+// ultrasonicThreshold
+// Detecting an ultrasonic sensor value below this threshold will cause the robot to stop
 int ultrasonicThreshold = 30;
 
+/*
+*	avoidCollision
+*	Actively watch the ultrasonic sensors to prevent a collision
+*/
 task avoidCollision()
 {
+	// Loop forever
 	while(true)
 	{
-		while(SensorValue[sUltraBack] < ultrasonicThreshold || SensorValue[sUltraFront] < ultrasonicThreshold)
+		// For as long as one or both ultrasonic sensors read below the threshold
+		while(ultraStrengthBack < ultrasonicThreshold || ultraStrengthFront < ultrasonicThreshold)
 		{
+			// Stop all motors
 			allMotorsTo(0);
+
+			// Print a STOP message to the NXT screen
 			nxtDisplayCenteredBigTextLine(1, "STOP");
+
+			// Stop all processes except this one (temporarily stops all other functions)
 			hogCPU();
 		}
+
+		// Start up all other processes again
 		releaseCPU();
 
 		//nxtDisplayTextLine(2, "%s", (SensorValue[sUltraBack] < ultrasonicThreshold)? "TRUE" : "FALSE");
--- Robot Code/Autonomous/headers/multiplexer.h
+++ Robot Code/Autonomous/headers/multiplexer.h
@@ -1,14 +1,17 @@
 // Include files to handle the multiplexer and all the different sensors
 #include "hitechnic-sensormux.h"
 #include "hitechnic-irseeker-v2.h"
+#include "lego-ultrasound.h"
 
 // Constants
 const bool LEFT = true;
 const bool RIGHT = false;
 
 // Macros to store the sensor addresses
-#define irLeft  	msensor_S2_1;			// IR Sensor, NXT 2 MUX 1
-#define irRight  	msensor_S2_2; 		// IR Sensor, NXT 2 MUX 2
+#define irLeft  	msensor_S2_1		// IR Sensor, NXT 2 MUX 1
+#define irRight  	msensor_S2_2 		// IR Sensor, NXT 2 MUX 2
+#define ultraBack	msensor_S2_3		// Ultrasonic, NXT 2 MUX 3
+#define ultraFront	msensor_S2_4		// Ultrasonic, NXT 2 MUX 4
 
 // Variables to store the sensor values
 int irStrengthLeft;
@@ -17,15 +20,27 @@ int irDirectionLeft;
 int irStrengthRight;
 int irDirectionRight;
 
+int ultraStrengthBack;
+int ultraStrengthFront;
+
+
 // Flag to turn on or off the IR seekers
 bool gettingIr = false;
 int dummy;
 
+/*
+*	getIRDirection
+*	Gets the overall direction of the chosen IR seeker
+*/
 int getIRDirection(tMUXSensor sensor)
 {
 	return HTIRS2readACDir(sensor);
 }
 
+/*
+*	getIRStrength
+*	Gets the overall strength of the chosen IR seeker
+*/
 int getIRStrength(tMUXSensor sensor)
 {
 	int strength;
@@ -33,14 +48,18 @@ int getIRStrength(tMUXSensor sensor)
 
 	HTIRS2readAllACStrength(sensor, acS1, acS2, acS3, acS4, acS5 );
 
-  strength = (acS1 > acS2) ? acS1 : acS2;
-  strength = (strength > acS3) ? strength : acS3;
-  strength = (strength > acS4) ? strength : acS4;
-  strength = (strength > acS5) ? strength : acS5;
+	strength = (acS1 > acS2) ? acS1 : acS2;
+	strength = (strength > acS3) ? strength : acS3;
+	strength = (strength > acS4) ? strength : acS4;
+	strength = (strength > acS5) ? strength : acS5;
 
 	return 	strength;
 }
 
+/*
+*	getIREnhanced
+*	Get both the direction and the strength of the chosen IR seeker
+*/
 void getIREnhanced(tMUXSensor sensor, bool LEFT_OR_RIGHT)
 {
 	if(LEFT_OR_RIGHT == LEFT)
@@ -49,6 +68,10 @@ void getIREnhanced(tMUXSensor sensor, bool LEFT_OR_RIGHT)
 		HTIRS2readEnhanced(sensor, irDirectionRight, irStrengthRight);
 }
 
+/*
+*	getIREnhanced
+*	Get both the direction and the strength of the chosen IR seeker
+*/
 void getIREnhanced(tSensors sensor, bool LEFT_OR_RIGHT)
 {
 	if(LEFT_OR_RIGHT == LEFT)
@@ -57,11 +80,10 @@ void getIREnhanced(tSensors sensor, bool LEFT_OR_RIGHT)
 		HTIRS2readEnhanced(sensor, irDirectionRight, irStrengthRight);
 }
 
-/////////////////////////////////////////////////////////////////////////
-//
-//	Update the sensor values
-//
-////////////////////////////////////////////////////////////////////////
+/*
+*	getSmux
+*	Update all the sensors attached to a multiplexer
+*/
 task getSmux()
 {
 	// Print a ready message
@@ -69,8 +91,6 @@ task getSmux()
 
 	// Loop forever
 	while (true){
-
-
 		// Only update the IR seeker variables when the IR seekers are turned on
 		// This keeps the debug stream clear
 		if(gettingIr){
@@ -79,5 +99,9 @@ task getSmux()
 			}
 
 		}
+
+		// Store the values of the ultrasonic sensors
+		ultraStrengthBack = USreadDist(ultraBack);
+		ultraStrengthFront = USreadDist(ultraFront);
 	}
 }
