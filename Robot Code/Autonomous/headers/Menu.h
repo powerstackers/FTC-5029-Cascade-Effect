de0c15e8ac1b990569378faf4c433f99efb87b25
samohtj <thomas.jonathan.c@gmail.com>
2015-01-01 14:28:29 -0500

    Filled out the license info
    Added LICENSE.txt, and put license information in all the source files.

--- Robot Code/Autonomous/headers/Menu.h
+++ Robot Code/Autonomous/headers/Menu.h
@@ -3,9 +3,20 @@
 *	Displays a dynamic menu to choose different options and settings
 *	during the autonomous period. All settings are saved to variables
 *	which can be accessed from another program.
+*	Copyright (C) 2015 Jonathan Thomas
 *
-*	THIS CODE IS PROVIDED AS-IS AND WITHOUT WARRANTY.
-*	THIS CODE IS OPEN FOR DISTRIBUTION AND MODIFICATION.
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 *	This code is modified from original code created by FTC team 3785
 *	https://github.com/hprobotics/ftcresources/tree/master/AutonomousChooser
--- Robot Code/Tele-Op/teleop.c
+++ Robot Code/Tele-Op/teleop.c
@@ -4,9 +4,20 @@
 /*
 *	Teleop.c
 *	Code to run our robot during the telop period.
+*	Copyright (C) 2015 Jonathan Thomas
 *
-*	THIS CODE IS PROVIDED AS-IS AND WITHOUT WARRANTY.
-*	THIS CODE IS OPEN FOR DISTRIBUTION AND MODIFICATION.
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 *	FTC Team #5029, The Powerstackers
 *	powerstackersftc.com
